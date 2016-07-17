9589265562530c532de506c091abafd1fb42756d
ArputharajRaja <arputharaj.raja@kaaylabs.com>
2016-07-17 14:07:35 +0530

    user name field added

--- app/src/main/java/com/example/jenkin/MainActivity.java
+++ app/src/main/java/com/example/jenkin/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.jenkin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText usernameEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        usernameEditText = (EditText) findViewById(R.id.username);
     }
 }
