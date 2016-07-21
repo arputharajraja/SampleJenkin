0090d1aec5cda5bad7702c21d89b2d6b809004cb
ArputharajRaja <arputharaj.raja@kaaylabs.com>
2016-07-21 20:11:43 +0530

    multidex added

---
+++ app/src/main/java/com/example/jenkin/MainApp.java
@@ -0,0 +1,14 @@
+package com.example.jenkin;
+
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by SRadhakrishnan on 21-07-2016.
+ */
+public class MainApp extends MultiDexApplication
+{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
